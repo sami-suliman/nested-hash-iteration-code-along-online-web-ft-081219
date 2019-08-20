25089b709feac999f9891f1a48b777c02ffada6f
sami suliman <sami.bco@gmail.com>
2019-08-20 11:25:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |att|
-      
+    contact_details_hash.each do |attribute, data|
+      data.delete 
     end 
     
   end
